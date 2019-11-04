cc16adca1555f5381d1fbef6ccbd5ac34e67b387
wpupdoot <hello@wpupdoot.com>
2019-11-04 06:54:35 +0000

    Added BG Image reference, added homepage link

--- snippet.php
+++ snippet.php
@@ -6,3 +6,23 @@ function custom_login_styles() {
     echo '<link rel="stylesheet" type="text/css" href="' . get_bloginfo('stylesheet_directory') . '/login.css" />';
 }
 add_action('login_head', 'custom_login_styles');
+
+/**
+ * Changes the login page URL to the website homepage
+ *
+ * @return string Home Page URL
+ */
+function custom_login_logo_url() {
+    return home_url();
+}
+add_filter("login_headerurl","custom_login_logo_url");
+
+/**
+ * Returns the Blog / Website name (set in Settings => General)
+ *
+ * @return string Website name
+ */
+function custom_login_logo_url_title() {
+    return get_bloginfo("name");
+}
+add_filter("login_headertitle","custom_login_logo_url_title");
